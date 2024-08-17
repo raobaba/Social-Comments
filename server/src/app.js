4aa7db654eb125ee114f5cb6cbf27fbffef9200f
Rajan Kumar <raorajan9576@gmail.com>
2024-08-17 11:55:29 +0530

    database connected successfully

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,13 @@
 const express = require("express");
 require("dotenv").config();
+const Connection = require('./config/database')
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
+Connection();
 
 app.get("/", (req, res) => {
   res.send("Server is Running! 🚀");
